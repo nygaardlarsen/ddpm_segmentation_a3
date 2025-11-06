482dc930f60e02306727a79d96ee2df8e8e9db00
Mathias Nygaard Iversen <mathias.nygaard.iversen@gmail.com>
2025-11-06 15:39:30 +0100

    remove unwanted files

--- ddpm-segmentation/scripts/ddpm/train_interpreter_elastic_transform_mix.sh
+++ ddpm-segmentation/scripts/ddpm/train_interpreter_elastic_transform_mix.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Kør flere eksperimenter sekventielt med forskellige noise-varianter
 
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 
-# Liste af JSON-filer for hvert eksperiment
 EXPERIMENTS=(
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_10pct.json"
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_20pct.json"
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_30pct.json"
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_10pct.json"
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_20pct.json"
-    # "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_30pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_10pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_20pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity1_30pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_10pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_20pct.json"
+    "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity3_30pct.json"
     "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity5_10pct.json"
     "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity5_20pct.json"
     "/work/ddpm-segmentation/experiments/horse_21/ddpm_elastic_transform_severity5_30pct.json"
--- ddpm-segmentation/scripts/ddpm/train_interpreter_saturate.sh
+++ ddpm-segmentation/scripts/ddpm/train_interpreter_saturate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Kør flere eksperimenter sekventielt med forskellige noise-varianter
 
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 
-# Liste af JSON-filer for hvert eksperiment
 EXPERIMENTS=(
     "/work/ddpm-segmentation/experiments/horse_21/ddpm_saturate_severity1.json"
     "/work/ddpm-segmentation/experiments/horse_21/ddpm_saturate_severity3.json"
